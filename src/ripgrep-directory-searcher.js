7b98dd1ed3f9e092db19964208a2ab3a36267235
Rafael Oleza <rafeca@github.com>
2019-06-04 12:17:38 +0200

    Take into account the includeHidden option in ripgrep scanner

--- src/ripgrep-directory-searcher.js
+++ src/ripgrep-directory-searcher.js
@@ -193,7 +193,7 @@ module.exports = class RipgrepDirectorySearcher {
   //   Each item in the array is a file/directory pattern, e.g., `src` to search in the "src"
   //   directory or `*.js` to search all JavaScript files. In practice, this often comes from the
   //   comma-delimited list of patterns in the bottom text input of the ProjectFindView dialog.
-  //   * `ignoreHidden` {boolean} whether to ignore hidden files.
+  //   * `includeHidden` {boolean} whether to ignore hidden files.
   //   * `excludeVcsIgnores` {boolean} whether to exclude VCS ignored paths.
   //   * `exclusions` {Array} similar to inclusions
   //   * `follow` {boolean} whether symlinks should be followed.
@@ -255,6 +255,10 @@ module.exports = class RipgrepDirectorySearcher {
 
     if (this.isMultilineRegexp(regexpStr)) {
       args.push('--multiline');
+    }
+
+    if (options.includeHidden) {
+      args.push('--hidden');
     }
 
     if (options.follow) {
